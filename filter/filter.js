90ba98e7c6a9cedfac829829d15c437adabe05b0
zid <dan_allegre@yahoo.com>
2017-07-16 14:30:26 -0500

    added a page for the filter module.

---
+++ filter/filter.js
@@ -0,0 +1,11 @@
+var parentdiv = document.getElementById("pageBody");
+
+
+var backButton = document.getElementById("backButton");
+var backButtonText = document.getElementById("backButtonText");
+
+window.onload = function(){
+  backButton.className = "opaque";
+  backButtonText.className = "opaque"; 
+};
+
